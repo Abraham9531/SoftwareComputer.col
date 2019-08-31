510c831519c7ece9581f11a04ea08980b790a58a
Abraham Jesus Ahumada <Ahumadanioa@gmail.com>
2019-08-31 22:55:29 +0200

    Se agrega la carpeta css mas sus archivos

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="Abraham">
   
-    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
     
     <title>SoftComp.col</title>
    
