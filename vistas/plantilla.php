e699797e2cddf9d9782ad295db42ea7c431c2488
Abraham Jesus Ahumada <Ahumadanioa@gmail.com>
2019-08-31 22:54:48 +0200

    Se agregan Ls carpetas y archivos de Plugins y archivo plantilla

---
+++ vistas/plantilla.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Abraham">
+  
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+    
+    <title>SoftComp.col</title>
+   
+    <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    
+    <link href="css/style.css" rel="stylesheet">
+    
+    <link href="css/colors/megna-dark.css" id="theme" rel="stylesheet">
+   
+</head>
