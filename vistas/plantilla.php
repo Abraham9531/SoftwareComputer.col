31f7b904cb8748163f08e1e8a256aaa9643e275d
Abraham Jesus Ahumada <ahumadanioa@gmail.com>
2019-09-21 00:30:16 -0500

    Se mueven las carpetas de estilo a plugins. Se ingresa el codigo a los archivos  de header body menu y footer .php y se borra el codigo basura en header.php

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -9,53 +9,66 @@
     <meta name="description" content="">
     <meta name="author" content="Abraham">
   
-    <link rel="icon" type="image/png" sizes="16x16" href="vistas/assets/images/favicon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="vistas/assets/images/Logos/01.png">
     
     <title>SoftComp.col</title>
    
-    <link href="vistas/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vistas/Plugins/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     
-    <link href="vistas/css/style.css" rel="stylesheet">
+    <link href="vistas/Plugins/css/style.css" rel="stylesheet">
     
-    <link href="vistas/css/colors/megna-dark.css" id="theme" rel="stylesheet">
+    <link href="vistas/Plugins/css/colors/red-dark.css" id="theme" rel="stylesheet">
    
 </head>
-<body>
-    
+<body class="fix-header card-no-border">
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <svg class="circular" viewBox="25 25 50 50">
+            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
+    </div>    
+
+<div id="main-wrapper">
+
 <?php 
 
 /*========================= Header ====================*/
-include"modulos/header.php";
+include"modulos/Header.php";
+
+/*========================= Menu ====================*/
+include"modulos/Menu.php";
 
 /*========================= Body ====================*/
-include"modulos/body.php";
+include"modulos/Body.php";
 
 /*========================= Footer ====================*/
-include"modulos/footer.php";
+include"modulos/Footer.php";
 
 /*========================= Copyright ====================*/
-include"modulos/copyright.php";
+include"modulos/Copyright.php";
 
  ?>
-    
+
+</div>
 </body>
 
-<script src="vistas/assets/plugins/jquery/jquery.min.js"></script>
+<script src="vistas/plugins/assets/plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap tether Core JavaScript -->
-    <script src="vistas/assets/plugins/bootstrap/js/tether.min.js"></script>
-    <script src="vistas/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="vistas/plugins/assets/plugins/bootstrap/js/tether.min.js"></script>
+    <script src="vistas/plugins/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
     <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="vistas/js/jquery.slimscroll.js"></script>
+    <script src="vistas/plugins/js/jquery.slimscroll.js"></script>
     <!--Wave Effects -->
-    <script src="vistas/js/waves.js"></script>
+    <script src="vistas/plugins/js/waves.js"></script>
     <!--Menu sidebar -->
-    <script src="vistas/js/sidebarmenu.js"></script>
+    <script src="vistas/plugins/js/sidebarmenu.js"></script>
     <!--stickey kit -->
-    <script src="vistas/assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <script src="vistas/plugins/assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
     <!--Custom JavaScript -->
-    <script src="vistas/js/custom.min.js"></script>
+    <script src="vistas/plugins/js/custom.min.js"></script>
     <!-- ============================================================== -->
     <!-- Style switcher -->
     <!-- ============================================================== -->
-    <script src="vistas/assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
+    <script src="vistas/plugins/assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
 </html>
